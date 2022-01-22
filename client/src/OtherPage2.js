1a59c5215d777acb10a3ad7a4fd6e13f3a4091e6
broffy <59145171+broffy@users.noreply.github.com>
2022-01-22 17:28:46 +0100

    Add files via upload

---
+++ client/src/OtherPage2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      Usluga zostala opracowana na bazie laboratorium 9
+      Zmienione rzeczy:
+    </div>
+  );
+};
