545611d250d99e23eaafd234efd11414206e8893
broffy <59145171+broffy@users.noreply.github.com>
2022-01-22 19:23:27 +0100

    Add files via upload

--- client/src/OtherPage2.js
+++ client/src/OtherPage2.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import working from './working.png';
 
 export default () => {
   return (
     <div>
+    
       Usluga zostala opracowana na bazie laboratorium 9
       Zmienione rzeczy:
+      Dockerfile : uzywanie uzytkownika node , dodanie wersji dev;
+      docker-compose: dodanie wersji dev i klienckiej;
+      Strona: dodanie podstron, zmiana css;
+      Port: zmiana portu 3050 na localhost;
+      
+      
+   
+     
+    <img src={working} width="700px" height="400px" alt="working" />
+    
     </div>
+   
   );
 };
